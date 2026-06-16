76c61a66f23a650ab1d1aaac3a3a6df227f51019
Paul Palis <115663393+fpaulpalis@users.noreply.github.com>
2026-06-17 03:14:58 +0800

    Simplify numberChecker logic by consolidating conditions

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,9 +1,7 @@
 function numberChecker(number) {
-  if (number === 1000 ||
-      number === 10
-  ) {
+  if (number >= 10) {
     return true;
-  } else if (number === 9 || number || 6) {
+  } else {
     return false;
   }
 }
